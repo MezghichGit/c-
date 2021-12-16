f9257ed6a3d35245ee08eb9a361036dce7dc8b39
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-12-16 10:33:29 +0100

    version 2 du=e l'exo

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include "Vecteur.h"
 #include "Matrice.h"
-
+using namespace std;
 int main()
 {
-    //Vecteur v(3);
-    //v.remplirVecteur();
-    Matrice m(3);
+    int d ;
+    cout<<"*******************************  Veuillez saisir la dimension *************************"<<endl;
+    cin>>d;
+
+    cout<<"*******************************  Saisi du vecteur *************************"<<endl;
+    Vecteur v(d);
+    v.remplirVecteur();
+    cout<<"*******************************  Saisi de la matrice *************************"<<endl;
+    Matrice m(d);
     m.remplirMatrice();
-   // cout << "Hello world!" << endl;
+    cout<<"*******************************  Mutliplication Matrice x Vecteur *************************"<<endl;
     return 0;
 }
